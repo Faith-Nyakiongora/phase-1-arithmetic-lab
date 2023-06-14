fba2562334f5219b00b8cd8bc8743669ee18edf9
+ <+>
2023-06-14 12:46:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31
+let num2 = 2
+    const multiply = num1*num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 2;
+    (Number.isInteger(random)) === true;
+    (random)!==0
+    
+let num3 = 22
+let num4 = 9
+    const mod = 22 % 9
+
+const max = (Math.max(5,10,20))
+    max === 20
+
+
